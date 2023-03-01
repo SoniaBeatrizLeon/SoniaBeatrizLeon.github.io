3b64c2b295bd492a7e835f2c8e0038474c08c6e1
SoniaBeatrizLeon <sobeleon19@gmail.com>
2023-02-28 23:07:27 -0300

    cambio de nombre de clase

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function mostrarSobreMi() {
-    console.log('solo un test');
     const parrafoSobreMi = document.getElementById("sobre");
     if(parrafoSobreMi.style.display === 'initial') {
         parrafoSobreMi.style.display = 'none';
